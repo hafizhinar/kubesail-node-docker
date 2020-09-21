3787b28bde9e95803190ca2fc8e8634bce711f19
Hafizhin Ar Rosyiid <hafizhinarrosyiid@Hafizhins-MacBook-Pro-2.local>
2020-09-21 12:32:33 +0700

    add new

---
+++ build/precache-manifest.08f34ebcf6c465461273c7b1b1b263bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9959d7aed360ea2e47d14510d419f738",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9524d00b4ee41fe6e772",
+    "url": "/static/css/main.bf3fe65a.chunk.css"
+  },
+  {
+    "revision": "44cfda680693a4c272ac",
+    "url": "/static/js/2.18c7eaf6.chunk.js"
+  },
+  {
+    "revision": "9524d00b4ee41fe6e772",
+    "url": "/static/js/main.0bde7c35.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a9b0546c1405ecbd299e97856b36027d",
+    "url": "/static/media/logos.a9b0546c.svg"
+  }
+]);
